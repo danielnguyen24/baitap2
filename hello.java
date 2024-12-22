01a99db323dab7181c6ee64da4d2b0fde9dcfd03
danielnguyen24 <truyendc23v7x217@dttx.ctu.edu.vn>
2024-12-22 16:18:17 +0700

    Chinh sua Chuong trinh Hello

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         // Hiển thị chuỗi "Hello World"
         System.out.println("Hello World");
+        System.out.println("Chinh sua chuong trinh Hello World");
     }
 }
