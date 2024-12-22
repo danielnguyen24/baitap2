ae31aa2e657b795ba7f0873953e01370fb15f989
danielnguyen24 <truyendc23v7x217@dttx.ctu.edu.vn>
2024-12-22 16:38:56 +0700

    Vong lap for tu 1 den 10

---
+++ vonglapfor.java
@@ -0,0 +1,8 @@
+public class vonglapfor {
+    public static void main(String[] args) {
+        // Hiển thị các số từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
